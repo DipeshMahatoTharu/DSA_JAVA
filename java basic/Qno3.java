65a4e31a2e83bff29d68fc0c01e25f30a58851f7
Dipesh Mahato <D.M.tharu@wlv.ac.uk>
2024-10-28 09:27:38 -0700

    q3 updated

---
+++ java basic/Qno3.java
@@ -0,0 +1,18 @@
+
+
+
+public class Qno3 {
+ public static void main(String[] args) {
+    int a,b,c;
+    a=12;
+    b=121;
+    c=11;
+    // print a 
+    System.out.println(a);
+    // print b
+    System.out.println(b);
+    // print c
+    System.out.println(c);
+ }
+    
+}
