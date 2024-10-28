c59e4444657f6d59692ac9d8ce770aaf85f78d06
Dipesh Mahato <D.M.tharu@wlv.ac.uk>
2024-10-28 08:32:30 -0700

    firstcode

--- java basic/Q1.java
+++
@@ -1,6 +0,0 @@
-//Write a Java program to print 'Hello' on screen and your name on a separate line
-public class Q1{
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
---
+++ java basic/Qno1.java
@@ -0,0 +1,14 @@
+// // You are familiar with producing output using JAVA In this task, you'll be required to write three messages in 
+// three separate lines. Write Geeks for Geeks. But wait a minute!! You need to write each word of it in a separate line.
+// In the function printIndividualLine(), output each word of Geeks for Geeks in a separate line.
+// Hint: System.out.println() might be useful here!
+
+
+
+public class Qno1{
+
+    public static void main(String[] args) {
+        System.out.println("Greek\nFor\nGreek");
+    }
+
+}
