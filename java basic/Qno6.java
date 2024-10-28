ae4f9b3e06f80368aad7b2673beeca5b28effd3a
Dipesh Mahato <D.M.tharu@wlv.ac.uk>
2024-10-28 10:55:35 -0700

    batman priting

--- java basic/Qno6.java
+++ java basic/Qno6.java
@@ -5,9 +5,30 @@
 // These operators produce either a true or a false as an output.
 
 
+
+import java.util.Scanner;
 public class Qno6 {
     public static void main(String[] args) {
+         Scanner sc =new Scanner(System.in);
+
+
+         System.out.print("Enter the first person name: ");
+            String Name1=sc.nextLine();
+            
+            
+
+         System.out.print("Enter the second person name: ");
+      
+            String Name2=sc.nextLine();
+
+
+        if(Name1.equals("Catwomen") && Name2.equals("Batman")){
+            System.out.println("oh love birds !!!");
+        }else{
+            System.out.println("who the hell are u ");
+        }
         
+            sc.close();
     }
     
 }
