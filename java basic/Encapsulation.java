65dbdadd5984d84bfef8797fdd42c07b3bb69d6a
Dipesh Mahato <D.M.tharu@wlv.ac.uk>
2024-10-30 17:59:16 -0700

    update

---
+++ java basic/Encapsulation.java
@@ -0,0 +1,37 @@
+// Encapsulation : 
+
+class human {
+
+    private int age  ;
+    private String name  ;
+     
+// to name it accessable 
+        public int getAge(){
+            return age ;
+        }
+        public void setAge(int a){
+            age =a;
+
+        }
+        //   same for name 
+        public String getName(){
+            return name;
+        }
+        public void setName(String n){
+            name=n;
+            
+            
+        }
+}
+
+
+public class Encapsulation {
+
+    public static void main(String[] args) {
+        human obj =new human();
+        obj.setAge(21);
+        obj.setName("Dipesh Mahato");
+        System.out.println(obj.getName());
+        System.out.println(obj.getAge());
+    }
+}
